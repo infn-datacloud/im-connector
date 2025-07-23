65911f1e440bbcb1a600f0d3ff654689bc0df8f4
Caterina Marcon <caterina.marcon@mi.infn.it>
2025-07-23 11:04:40 +0200

    Fixed Authorization header

--- api/im.py
+++ api/im.py
@@ -1,10 +1,4 @@
-import dataclasses
-
 import requests
-
-from config import get_settings
-
-settings = get_settings()
 
 def create_k8s_deployment(im_url, jwt, tosca_template):
     im_jwt = ""
@@ -12,10 +6,14 @@ def create_k8s_deployment(im_url, jwt, tosca_template):
     k8s_jwt = ""
     k8s_host = ""
 
+    header_list = [
+        f"type = InfrastructureManager; token = {im_jwt}",
+        f"id = k8sMilano; type = 'Kubernetes'; host = {k8s_host}; token = {k8s_jwt}"
+    ]
+
     headers = {
         "Content-Type": "text/yaml",
-        "Authorization": f"type = InfrastructureManager; token = {im_jwt}\nid = k8sMilano; type = 'Kubernetes'; host = {k8s_host}; token = {k8s_jwt}"
+        "Authorization": "\\n".join(header_list)
     }
 
     response = requests.post(im_url, data=tosca_template, headers=headers)
-
